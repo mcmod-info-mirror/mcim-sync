a7051a7196a286e7e66c4efc30739375cc9f5249
z0z0r4 <z0z0r4@outlook.com>
2025-01-24 15:46:22 +0800

    rechore: model
    用 api_wrapper 的 Model，然后封装

--- models/database/curseforge.py
+++ models/database/curseforge.py
@@ -2,6 +2,11 @@ from odmantic import Model, Field, EmbeddedModel
 from pydantic import BaseModel, field_serializer, model_validator
 from typing import List, Optional
 from datetime import datetime
+from curseforge_api_wrapper.models import (
+    File as _File,
+    Mod as _Mod,
+    Fingerprint as _Fingerprint,
+)
 
 
 class FileDependencies(BaseModel):
@@ -17,34 +22,15 @@ class FileSortableGameVersions(BaseModel):
     gameVersionTypeId: Optional[int] = None
 
 
-"""
-1=Sha1
-2=Md5
-"""
-
-
 class Hash(BaseModel):
     value: str
     algo: int
-
-
-{"id": 0, "name": "string", "url": "string"}
 
 
 class Author(BaseModel):
     id: int
     name: str
     url: Optional[str] = None
-
-
-{
-    "id": 0,
-    "modId": 0,
-    "title": "string",
-    "description": "string",
-    "thumbnailUrl": "string",
-    "url": "string",
-}
 
 
 class Logo(BaseModel):
@@ -54,21 +40,6 @@ class Logo(BaseModel):
     description: Optional[str] = None
     thumbnailUrl: Optional[str] = None
     url: Optional[str] = None
-
-
-{
-    "id": 0,
-    "gameId": 0,
-    "name": "string",
-    "slug": "string",
-    "url": "string",
-    "iconUrl": "string",
-    "dateModified": "2019-08-24T14:15:22Z",
-    "isClass": True,
-    "classId": 0,
-    "parentCategoryId": 0,
-    "displayIndex": 0,
-}
 
 
 class Category(BaseModel):
@@ -85,29 +56,11 @@ class Category(BaseModel):
     displayIndex: Optional[int] = None
 
 
-{
-    "websiteUrl": "string",
-    "wikiUrl": "string",
-    "issuesUrl": "string",
-    "sourceUrl": "string",
-}
-
-
 class Links(BaseModel):
     websiteUrl: Optional[str] = None
     wikiUrl: Optional[str] = None
     issuesUrl: Optional[str] = None
     sourceUrl: Optional[str] = None
-
-
-{
-    "id": 0,
-    "modId": 0,
-    "title": "string",
-    "description": "string",
-    "thumbnailUrl": "string",
-    "url": "string",
-}
 
 
 class ScreenShot(BaseModel):
@@ -119,78 +72,39 @@ class ScreenShot(BaseModel):
     url: Optional[str] = None
 
 
-{"name": "string", "fingerprint": 0}
-
-
 class Module(BaseModel):
     name: Optional[str] = None
     fingerprint: Optional[int] = None
 
 
-{
-    "id": 0,
-    "gameId": 0,
-    "modId": 0,
-    "isAvailable": True,
-    "displayName": "string",
-    "fileName": "string",
-    "releaseType": 1,
-    "fileStatus": 1,
-    "hashes": [{"value": "string", "algo": 1}],
-    "fileDate": "2019-08-24T14:15:22Z",
-    "fileLength": 0,
-    "downloadCount": 0,
-    "fileSizeOnDisk": 0,
-    "downloadUrl": "string",
-    "gameVersions": ["string"],
-    "sortableGameVersions": [
-        {
-            "gameVersionName": "string",
-            "gameVersionPadded": "string",
-            "gameVersion": "string",
-            "gameVersionReleaseDate": "2019-08-24T14:15:22Z",
-            "gameVersionTypeId": 0,
-        }
-    ],
-    "dependencies": [{"modId": 0, "relationType": 1}],
-    "exposeAsAlternative": True,
-    "parentProjectFileId": 0,
-    "alternateFileId": 0,
-    "isSerlyAccessContent": True,
-    "earlyAccessEndDate": "2019-08-24T14:15:22Z",
-    "fileFingerprint": 0,
-    "modules": [{"name": "string", "fingerprint": 0}],
-}
-
-
-class File(Model):
+class File(Model, _File):
     id: int = Field(primary_field=True, index=True)
-    gameId: int
-    modId: int = Field(index=True)
-    isAvailable: Optional[bool] = None
-    displayName: Optional[str] = None
-    fileName: Optional[str] = None
-    releaseType: Optional[int] = None
-    fileStatus: Optional[int] = None
-    hashes: Optional[List[Hash]] = None
-    fileDate: Optional[datetime] = None
-    fileLength: Optional[int] = None
-    downloadCount: Optional[int] = None
-    fileSizeOnDisk: Optional[int] = None
-    downloadUrl: Optional[str] = None
-    gameVersions: Optional[List[str]] = None
-    sortableGameVersions: Optional[List[FileSortableGameVersions]] = None
-    dependencies: Optional[List[FileDependencies]] = None
-    exposeAsAlternative: Optional[bool] = None
-    parentProjectFileId: Optional[int] = None
-    alternateFileId: Optional[int] = None
-    isServerPack: Optional[bool] = None
-    serverPackFileId: Optional[int] = None
-    isEarlyAccessContent: Optional[bool] = None
-    earlyAccessEndDate: Optional[datetime] = None
-    fileFingerprint: Optional[int] = None
-    modules: Optional[List[Module]] = None
-    
+    # gameId: int
+    # modId: int = Field(index=True)
+    # isAvailable: Optional[bool] = None
+    # displayName: Optional[str] = None
+    # fileName: Optional[str] = None
+    # releaseType: Optional[int] = None
+    # fileStatus: Optional[int] = None
+    # hashes: Optional[List[Hash]] = None
+    # fileDate: Optional[datetime] = None
+    # fileLength: Optional[int] = None
+    # downloadCount: Optional[int] = None
+    # fileSizeOnDisk: Optional[int] = None
+    # downloadUrl: Optional[str] = None
+    # gameVersions: Optional[List[str]] = None
+    # sortableGameVersions: Optional[List[FileSortableGameVersions]] = None
+    # dependencies: Optional[List[FileDependencies]] = None
+    # exposeAsAlternative: Optional[bool] = None
+    # parentProjectFileId: Optional[int] = None
+    # alternateFileId: Optional[int] = None
+    # isServerPack: Optional[bool] = None
+    # serverPackFileId: Optional[int] = None
+    # isEarlyAccessContent: Optional[bool] = None
+    # earlyAccessEndDate: Optional[datetime] = None
+    # fileFingerprint: Optional[int] = None
+    # modules: Optional[List[Module]] = None
+
     need_to_cache: bool = True  # 不缓存 Mod 以外的东西，在获得 mod 类型的时候设置
     file_cdn_cached: bool = False
     sha1: Optional[str] = None
@@ -201,6 +115,7 @@ class File(Model):
     model_config = {
         "collection": "curseforge_files",
     }
+
 
 class FileInfo(BaseModel):
     id: int
@@ -233,16 +148,6 @@ class FileInfo(BaseModel):
     md5: Optional[str] = None
 
 
-{
-    "gameVersion": "string",
-    "fileId": 0,
-    "filename": "string",
-    "releaseType": 1,
-    "gameVersionTypeId": 0,
-    "modLoader": 0,
-}
-
-
 class FileIndex(BaseModel):
     gameVersion: Optional[str] = None
     fileId: int
@@ -252,7 +157,7 @@ class FileIndex(BaseModel):
     modLoader: Optional[int] = None
 
 
-class Mod(Model):
+class Mod(Model, _Mod):
     id: int = Field(primary_field=True, index=True)
     gameId: Optional[int] = None
     name: Optional[str] = None
@@ -289,9 +194,6 @@ class Mod(Model):
     }
 
 
-{"pagination": {"index": 0, "pageSize": 0, "resultCount": 0, "totalCount": 0}}
-
-
 class Pagination(BaseModel):
     index: int
     pageSize: int
@@ -300,10 +202,10 @@ class Pagination(BaseModel):
 
 
 # TODO: add latestFiles Mod reference but not refresh while refreshing File
-class Fingerprint(Model):
+class Fingerprint(Model, _Fingerprint):
     id: int = Field(primary_field=True, index=True)
-    file: FileInfo
-    latestFiles: List[FileInfo]
+    # file: FileInfo
+    # latestFiles: List[FileInfo]
 
     sync_at: datetime = Field(default_factory=datetime.utcnow)
 
--- models/database/modrinth.py
+++ models/database/modrinth.py
@@ -1,18 +1,25 @@
 from odmantic import Model, Field, EmbeddedModel
 from pydantic import BaseModel, field_serializer, field_validator, model_validator
-
 from typing import List, Optional, Union
 from datetime import datetime
+from modrinth_api_wrapper.models import (
+    Project as _Project,
+    Version as _Version,
+    File as _File,
+)
+
 
 class DonationUrl(BaseModel):
     id: Optional[str] = None
     platform: Optional[str] = None
     url: Optional[str] = None
 
+
 class License(BaseModel):
     id: Optional[str] = None
     name: Optional[str] = None
     url: Optional[str] = None
+
 
 class GalleryItem(BaseModel):
     url: str
@@ -22,41 +29,42 @@ class GalleryItem(BaseModel):
     created: datetime
     ordering: Optional[int] = None
 
-class Project(Model):
+
+class Project(Model, _Project):
     id: str = Field(primary_field=True, index=True)
     slug: str = Field(index=True)
-    title: Optional[str] = None
-    description: Optional[str] = None
-    categories: Optional[List[str]] = None
-    client_side: Optional[str] = None
-    server_side: Optional[str] = None
-    body: Optional[str] = None
-    status: Optional[str] = None
-    requested_status: Optional[str] = None
-    additional_categories: Optional[List[str]] = None
-    issues_url: Optional[str] = None
-    source_url: Optional[str] = None
-    wiki_url: Optional[str] = None
-    discord_url: Optional[str] = None
-    donation_urls: Optional[List[DonationUrl]] = None
-    project_type: Optional[str] = None
-    downloads: Optional[int] = None
-    icon_url: Optional[str] = None
-    color: Optional[int] = None
-    thread_id: Optional[str] = None
-    monetization_status: Optional[str] = None
-    team: str
-    body_url: Optional[str] = None
-    published: datetime
-    updated: datetime
-    approved: Optional[datetime] = None
-    queued: Optional[datetime] = None
-    followers: int
-    license: Optional[License] = None
-    versions: Optional[List[str]] = None
-    game_versions: Optional[List[str]] = None
-    loaders: Optional[List[str]] = None
-    gallery: Optional[List[GalleryItem]] = None
+    # title: Optional[str] = None
+    # description: Optional[str] = None
+    # categories: Optional[List[str]] = None
+    # client_side: Optional[str] = None
+    # server_side: Optional[str] = None
+    # body: Optional[str] = None
+    # status: Optional[str] = None
+    # requested_status: Optional[str] = None
+    # additional_categories: Optional[List[str]] = None
+    # issues_url: Optional[str] = None
+    # source_url: Optional[str] = None
+    # wiki_url: Optional[str] = None
+    # discord_url: Optional[str] = None
+    # donation_urls: Optional[List[DonationUrl]] = None
+    # project_type: Optional[str] = None
+    # downloads: Optional[int] = None
+    # icon_url: Optional[str] = None
+    # color: Optional[int] = None
+    # thread_id: Optional[str] = None
+    # monetization_status: Optional[str] = None
+    # team: str
+    # body_url: Optional[str] = None
+    # published: datetime
+    # updated: datetime
+    # approved: Optional[datetime] = None
+    # queued: Optional[datetime] = None
+    # followers: int
+    # license: Optional[License] = None
+    # versions: Optional[List[str]] = None
+    # game_versions: Optional[List[str]] = None
+    # loaders: Optional[List[str]] = None
+    # gallery: Optional[List[GalleryItem]] = None
 
     translated_description: Optional[str] = None
 
@@ -65,6 +73,7 @@ class Project(Model):
     model_config = {
         "collection": "modrinth_projects",
     }
+
 
 class Dependencies(BaseModel):
     version_id: Optional[str] = None
@@ -79,22 +88,23 @@ class Hashes(EmbeddedModel):
 
 
 # TODO: Add Version reference directly but not query File again
-class File(Model):
+class File(Model, _File):
     hashes: Hashes = Field(primary_field=True)
-    url: str
-    filename: str
-    primary: bool
-    size: int
-    file_type: Optional[str] = None
+    # url: str
+    # filename: str
+    # primary: bool
+    # size: int
+    # file_type: Optional[str] = None
 
-    version_id: Optional[str] #  = Field(index=True)  # 有可能没有该 file...
-    project_id: Optional[str] #  = Field(index=True)
+    version_id: Optional[str]  #  = Field(index=True)  # 有可能没有该 file...
+    project_id: Optional[str]  #  = Field(index=True)
 
     file_cdn_cached: Optional[bool] = False
 
     sync_at: datetime = Field(default_factory=datetime.utcnow)
 
     model_config = {"collection": "modrinth_files"}
+
 
 class FileInfo(BaseModel):
     hashes: Hashes
@@ -104,25 +114,26 @@ class FileInfo(BaseModel):
     size: int
     file_type: Optional[str] = None
 
-class Version(Model):
+
+class Version(Model, _Version):
     id: str = Field(primary_field=True, index=True)
     project_id: str = Field(index=True)
-    slug: Optional[str] = None
-    name: Optional[str] = None
-    version_number: Optional[str] = None
-    changelog: Optional[str] = None
-    dependencies: Optional[List[Dependencies]] = None
-    game_versions: Optional[List[str]] = None
-    version_type: Optional[str] = None
-    loaders: Optional[List[str]] = None
-    featured: Optional[bool] = None
-    status: Optional[str] = None
-    requested_status: Optional[str] = None
-    author_id: str
-    date_published: datetime
-    downloads: int
-    changelog_url: Optional[str] = None  # Deprecated
-    files: List[FileInfo]
+    # slug: Optional[str] = None
+    # name: Optional[str] = None
+    # version_number: Optional[str] = None
+    # changelog: Optional[str] = None
+    # dependencies: Optional[List[Dependencies]] = None
+    # game_versions: Optional[List[str]] = None
+    # version_type: Optional[str] = None
+    # loaders: Optional[List[str]] = None
+    # featured: Optional[bool] = None
+    # status: Optional[str] = None
+    # requested_status: Optional[str] = None
+    # author_id: str
+    # date_published: datetime
+    # downloads: int
+    # changelog_url: Optional[str] = None  # Deprecated
+    # files: List[FileInfo]
 
     sync_at: datetime = Field(default_factory=datetime.utcnow)
 
