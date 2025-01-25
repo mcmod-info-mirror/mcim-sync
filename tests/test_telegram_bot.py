55db08983f04aebf28be12f4a1e2525ee08a62a2
z0z0r4 <z0z0r4@outlook.com>
2025-01-25 18:50:08 +0800

    fix: update tests to use config for chat_id and add pytest skip marker

--- tests/test_telegram_bot.py
+++ tests/test_telegram_bot.py
@@ -1,4 +1,4 @@
-import os
+import pytest
 from telegram.helpers import escape_markdown
 
 from utils.telegram import send_message_sync, pin_message, StatisticsNotification
@@ -6,33 +6,32 @@ from config import Config
 
 config = Config.load()
 
-chat_id = os.getenv("CHAT_ID")
-if chat_id is None:
-    raise ValueError("CHAT_ID is not set")
-
-config.telegram_bot = True
-config.chat_id = chat_id
-
 pin_message_id: int = 0
 
+skip_mark = pytest.mark.skipif(
+    not config.telegram_bot, reason="config.telegram_bot is False, skip tests"
+)
 
+@skip_mark
 def test_send_message_sync():
-    message_id = send_message_sync("Hello, World!", chat_id=chat_id)  # test PlainText
+    message_id = send_message_sync("Hello, World!", chat_id=config.chat_id)  # test PlainText
     assert isinstance(message_id, int)
     message_id = send_message_sync(
-        escape_markdown("Hello, World!", version=2), parse_mode="MarkdownV2", chat_id=chat_id
+        escape_markdown("Hello, World!", version=2), parse_mode="MarkdownV2", chat_id=config.chat_id
     )  # test MarkdownV2
     global pin_message_id
     pin_message_id = message_id
     assert isinstance(message_id, int)
 
 
+@skip_mark
 def test_pin_message():
     if pin_message_id == 0:
         raise ValueError("pin_message_id is not set")
-    result = pin_message(pin_message_id, chat_id=chat_id)
+    result = pin_message(pin_message_id, chat_id=config.chat_id)
     assert result == True
 
 
+@skip_mark
 def test_send_message_static():
     assert isinstance(StatisticsNotification.send_to_telegram(), int)
