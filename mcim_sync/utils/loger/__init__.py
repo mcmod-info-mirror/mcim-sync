24ce4b437d0d42bfa680b0ca36a41ddcdc619160
z0z0r4 <z0z0r4@outlook.com>
2025-02-02 16:05:05 +0800

    remove: 移除不必要的 InterceptHandler 类

--- mcim_sync/utils/loger/__init__.py
+++ mcim_sync/utils/loger/__init__.py
@@ -1,5 +1,3 @@
-from types import FrameType
-from typing import cast
 from loguru import logger
 import os
 import sys
@@ -86,26 +84,6 @@ class Logger:
 
     def get_logger(self):
         return self.logger
-
-
-class InterceptHandler(logging.Handler):
-    def emit(self, record: logging.LogRecord) -> None:  # pragma: no cover
-        # Get corresponding Loguru level if it exists
-        try:
-            level = logger.level(record.levelname).name
-        except ValueError:
-            level = str(record.levelno)
-
-        # Find caller from where originated the logged message
-        frame, depth = logging.currentframe(), 2
-        while frame.f_code.co_filename == logging.__file__:  # noqa: WPS609
-            frame = cast(FrameType, frame.f_back)
-            depth += 1
-
-        logger.opt(depth=depth, exception=record.exc_info).log(
-            level,
-            record.getMessage(),
-        )
 
 
 Loggers = Logger()
