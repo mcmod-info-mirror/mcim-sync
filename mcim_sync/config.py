ec50a05556167b9f9cc4fe6098b6f8f85ae1410a
z0z0r4 <z0z0r4@outlook.com>
2025-07-14 14:23:52 +0800

    fix: missing curseforge_refresh_full in JobConfigModel

--- mcim_sync/config.py
+++ mcim_sync/config.py
@@ -24,6 +24,7 @@ class RedisConfigModel(BaseModel):
 
 class JobConfigModel(BaseModel):
     curseforge_refresh: bool = True
+    curseforge_refresh_full: bool = True
     modrinth_refresh: bool = True
     sync_curseforge_by_queue: bool = True
     sync_curseforge_by_search: bool = True
