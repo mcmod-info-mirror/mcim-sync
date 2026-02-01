d258d8d6424ba3ebf5d06dadd88e7fed60dc339c
z0z0r4 <z0z0r4@outlook.com>
2026-02-01 18:24:09 +0800

    fix: 添加对 MongoDB 连接的异常处理和错误提示

--- mcim_sync/database/mongodb.py
+++ mcim_sync/database/mongodb.py
@@ -1,11 +1,18 @@
 from odmantic import SyncEngine
 from pymongo import MongoClient
 from pymongo.database import Database
-
+from pymongo import errors as pymongo_errors
 from mcim_sync.config import Config
 
 _mongodb_config = Config.load().mongodb
 
+def ping_mongodb_client(client: MongoClient) -> bool:
+    try:
+        client.admin.command('ping')
+        return True
+    except Exception as e:
+        print(f"An error occurred: {e}, failed to connect to MongoDB")
+        return False
 
 def init_mongodb_syncengine() -> SyncEngine:
     """
@@ -13,12 +20,19 @@ def init_mongodb_syncengine() -> SyncEngine:
     :return:
     """
     global sync_mongo_engine
-    sync_mongo_engine = SyncEngine(
-        client=MongoClient(
+
+    client = MongoClient(
             f"mongodb://{_mongodb_config.user}:{_mongodb_config.password}@{_mongodb_config.host}:{_mongodb_config.port}"
             if _mongodb_config.auth
             else f"mongodb://{_mongodb_config.host}:{_mongodb_config.port}"
-        ),
+        )
+    ping_result = ping_mongodb_client(client)
+    if not ping_result:
+        client.close()
+        exit(1)
+
+    sync_mongo_engine = SyncEngine(
+        client=client,
         database=_mongodb_config.database,
     )
     return sync_mongo_engine
@@ -30,8 +44,12 @@ def init_mongodb_raw_client() -> Database:
         f"mongodb://{_mongodb_config.user}:{_mongodb_config.password}@{_mongodb_config.host}:{_mongodb_config.port}"
         if _mongodb_config.auth
         else f"mongodb://{_mongodb_config.host}:{_mongodb_config.port}"
-    )[_mongodb_config.database]
-    return raw_mongo_client
+    )
+    ping_result = ping_mongodb_client(raw_mongo_client)
+    if not ping_result:
+        raw_mongo_client.close()
+        exit(1)
+    return raw_mongo_client[_mongodb_config.database]
 
 
 sync_mongo_engine: SyncEngine = init_mongodb_syncengine()
